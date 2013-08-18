0cd3c6b8fd38164e76982bfb9f524db41f427227
dhammack <dhammack8@gmail.com>
2013-08-18 15:34:33 -0400

    bugfix

--- worddoesntbelong.py
+++ worddoesntbelong.py
@@ -2,6 +2,7 @@
 #use word2vec for vectorization
 #compute the mean of the vectors, find the one which is furthest!
 import numpy as np
+import os
 
 def load_word2vec(dir):
 	#new: since github has a 100M limit, load from a bunch of files in 
@@ -10,7 +11,7 @@ def load_word2vec(dir):
 	for path in os.listdir(dir):
 		iword2vec = {}
 		#load the word2vec features.
-		with open(path, 'r') as fin:
+		with open(os.path.join(dir,path), 'r') as fin:
 			next(fin) #skip information on first line
 			for line in fin:
 				items = line.replace('\r','').replace('\n','').split(' ')
@@ -21,7 +22,7 @@ def load_word2vec(dir):
 		
 		word2vec.update(iword2vec)
 		
-    return word2vec
+	return word2vec
     
 def get_furthest_word(words, word2vect):
 	vectlist = []
@@ -38,12 +39,12 @@ def get_furthest_word(words, word2vect):
 def main():
 	print "Hello human. I attempt to find a word in a list which doesn't belong.\n\n"
 	print 'loading knowledge from Wikipedia...should take 10-20 seconds'
-	word2vec = load_word2vec('vectors.txt')
+	word2vec = load_word2vec('vectors')
 	print 'Type several words separated by spaces. The more words you enter, the better I can guess.'
 	while (True):
 		words = raw_input('->').lower().split(' ')
-		print 'I think',get_furthest_word(words, word2vec),'doesnt belong in this list!'
-		a = raw_input('press ENTER to go again, CTRL+C to exit')
+		print 'I think',get_furthest_word(words, word2vec),'doesnt belong in this list!\n'
+		
 		
 if __name__ == '__main__':
     main()
